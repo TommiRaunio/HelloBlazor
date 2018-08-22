6decd3876c175eaf9bd93e3fac995eca4ca8ac08
Tommi Raunio <tommi.raunio@solita.fi>
2018-08-22 13:35:45 +0300

    Blazor technology demo

---
+++ Blazor/Models/MockStateMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Models
+{
+    public static class MockStateMachine
+    {
+        private static string _mood;
+        public static string Mood
+        {
+            get => _mood;
+            set
+            {
+                _mood = value;
+                StateChanged?.Invoke(null, EventArgs.Empty);
+                Console.WriteLine(_mood);
+            }
+        }
+
+        public static event EventHandler StateChanged;
+    }    
+}
